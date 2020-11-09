26f33f83b2ed39f8f23539b1c60a22c17dd38c00
Dustin Wurtz <dustinwurtz@Dustins-MacBook-Pro.local>
2020-11-08 18:20:01 -0800

    Switch between month and week views

--- src/View_Controller/CalendarMonthController.java
+++ src/View_Controller/CalendarMonthController.java
@@ -1,13 +1,22 @@
 package View_Controller;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
 import javafx.scene.control.ButtonType;
 import javafx.stage.Modality;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
+import java.io.IOException;
 import java.util.Optional;
 
 public class CalendarMonthController {
+    public Button WeekViewButton;
+
     public void exitHandler(ActionEvent actionEvent) {
         Alert confirmDelete = new Alert(Alert.AlertType.CONFIRMATION);
         confirmDelete.initModality(Modality.NONE);
@@ -19,5 +28,19 @@ public class CalendarMonthController {
         if(userChoice.get() == ButtonType.OK) {
             System.exit(0);
         }
+    }
+
+    public void weekHandler(ActionEvent actionEvent) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("CalendarWeek.fxml"));
+        Parent rootAddPart = (Parent) fxmlLoader.load();
+        Stage stage = new Stage();
+        stage.initModality(Modality.APPLICATION_MODAL);
+        stage.initStyle(StageStyle.UNDECORATED);
+        stage.setTitle("Calendar Week");
+        stage.setScene(new Scene(rootAddPart));
+        stage.show();
+
+        Stage currentStage = (Stage) WeekViewButton.getScene().getWindow();
+        currentStage.close();
     }
 }
