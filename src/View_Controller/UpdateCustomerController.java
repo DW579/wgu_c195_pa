376cd8cfd0b8274eff6ee3d755fcb7d69e07f7b7
Dustin Wurtz <dustinwurtz@Dustins-MacBook-Pro.local>
2020-12-11 16:16:04 -0800

    Feature to update customer

--- src/View_Controller/UpdateCustomerController.java
+++ src/View_Controller/UpdateCustomerController.java
@@ -4,8 +4,12 @@ import javafx.event.ActionEvent;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
+import utils.DBConnection;
 
 import java.io.IOException;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class UpdateCustomerController {
     public TextField IdField;
@@ -21,7 +25,76 @@ public class UpdateCustomerController {
     public Button CloseButton;
 
     public void selectedCustomer(int customerId) throws IOException {
-        System.out.println(customerId);
+        int addressId = 0;
+        int cityId = 0;
+        int countryId = 0;
+
+        IdField.setText(Integer.toString(customerId));
+
+        // Look up customer data in customer table
+        try {
+            Statement dbConnectionStatement = DBConnection.getConnection().createStatement();
+            String queryForSelectedCustomer = "SELECT * FROM customer WHERE customerId=" + Integer.toString(customerId);
+            ResultSet rs = dbConnectionStatement.executeQuery(queryForSelectedCustomer);
+
+            rs.next();
+
+            NameField.setText(rs.getString("customerName"));
+
+            addressId = rs.getInt("addressId");
+        }
+        catch (SQLException e) {
+            System.out.println("SQLException error: " + e.getMessage());
+        }
+
+        // Look up address data in address table
+        try {
+            Statement dbConnectionStatement = DBConnection.getConnection().createStatement();
+            String queryForSelectedAddress = "SELECT * FROM address WHERE addressId=" + Integer.toString(addressId);
+            ResultSet rs = dbConnectionStatement.executeQuery(queryForSelectedAddress);
+
+            rs.next();
+
+            AddressField.setText(rs.getString("address"));
+            Address2Field.setText(rs.getString("address2"));
+            ZipCodeField.setText(rs.getString("postalCode"));
+            PhoneField.setText(rs.getString("phone"));
+
+            cityId = rs.getInt("cityId");
+        }
+        catch (SQLException e) {
+            System.out.println("SQLException error: " + e.getMessage());
+        }
+
+        // Look up city data in city table
+        try {
+            Statement dbConnectionStatement = DBConnection.getConnection().createStatement();
+            String queryForSelectedCity = "SELECT * FROM city WHERE cityId=" + Integer.toString(cityId);
+            ResultSet rs = dbConnectionStatement.executeQuery(queryForSelectedCity);
+
+            rs.next();
+
+            CityField.setText(rs.getString("city"));
+
+            countryId = rs.getInt("countryId");
+        }
+        catch (SQLException e) {
+            System.out.println("SQLException error: " + e.getMessage());
+        }
+
+        // Look up country data in country table
+        try {
+            Statement dbConnectionStatement = DBConnection.getConnection().createStatement();
+            String queryForSelectedCountry = "SELECT * FROM country WHERE countryId=" + Integer.toString(countryId);
+            ResultSet rs = dbConnectionStatement.executeQuery(queryForSelectedCountry);
+
+            rs.next();
+
+            CountryField.setText(rs.getString("country"));
+        }
+        catch (SQLException e) {
+            System.out.println("SQLException error: " + e.getMessage());
+        }
     }
 
     public void IdHandler(ActionEvent actionEvent) {
